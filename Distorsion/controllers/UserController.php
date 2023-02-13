eaa1008c4f0468144f419a41a154aade23181c3e
Bluebender <olliviev@gmail.com>
2023-02-13 14:00:03 +0000

    Sauvegarde de 15h

--- Distorsion/controllers/UserController.php
+++ Distorsion/controllers/UserController.php
@@ -9,57 +9,68 @@ class UserController extends AbstractController{
     {
         $this->manager = new UserManager("arnauddeletre_Distorsion", "3306", "db.3wa.io","arnauddeletre","900979afbcfa4468bcb42cce8d75b844");
     }
-    
-    
-    public function index()
-    {
-       $this->render("accueil", []);
-    }
-    
-    public function register(array $user)
-    {
-        if(isset($_POST["username"]) && !empty($_POST["username"])
-        && isset($_POST["registerEmail"]) && !empty($_POST["registerEmail"])
-        && isset($_POST["registerPassword"]) && !empty($_POST["registerPassword"])
-        && isset($_POST["confirm-password"]) && !empty($_POST["confirm-password"]))
-        {
-            if($_POST["registerPassword"] === $_POST["confirm-password"])
-            {
-        
-            $hashPwd=password_hash($_POST["registerPassword"], PASSWORD_DEFAULT);
-        
-            $newUser=new User($_POST['username'],$_POST['registerEmail'], $hashPwd);
-        
-            saveUser($newUser);
-            $this->render("bienvenu", []);
-            }
-    
-            else 
-            {
-                echo "Les mots de passe sont différents !";
-            }
-    
-            if(loadUser($_POST['registerEmail']===null))
-            {
-                echo "Email déjà utilisé";
-            }
 
-        }   
-        
-        else if(isset($_POST['username']) && empty($_POST['username']))
-        {
-            echo "Veuillez saisir un Pseudo";
-        }
-        else if(isset($_POST['registerEmail']) && empty($_POST['registerEmail']))
-        {
-            echo "Veuillez saisir un Email";
-        }
-        
-        $users=$this->manager->saveUser($newUser);
-        
-        $this->render("bienvenu", ["users"=>$users]);
+    public function indexDisplay()
+    {
+        $this->render("home", []);
     }
     
+    public function registerDisplay()
+    {
+        $this->render("register", []);
+    }
+    
+    public function loginDisplay()
+    {
+        $this->render("login", []);
+    }
+
+    public function welcomeDisplay()
+    {
+        $this->render("welcome", []);
+    }
+
+
+        // if(isset($_POST["username"]) && !empty($_POST["username"])
+        // && isset($_POST["registerEmail"]) && !empty($_POST["registerEmail"])
+        // && isset($_POST["registerPassword"]) && !empty($_POST["registerPassword"])
+        // && isset($_POST["confirm-password"]) && !empty($_POST["confirm-password"]))
+        // {
+        //     if($_POST["registerPassword"] === $_POST["confirm-password"])
+        //     {
+        
+        //     $hashPwd=password_hash($_POST["registerPassword"], PASSWORD_DEFAULT);
+        
+        //     $newUser=new User($_POST['username'],$_POST['registerEmail'], $hashPwd);
+        
+        //     saveUser($newUser);
+        //     $this->render("bienvenu", []);
+        //     }
+    
+        //     else 
+        //     {
+        //         echo "Les mots de passe sont différents !";
+        //     }
+    
+        //     if(loadUser($_POST['registerEmail']===null))
+        //     {
+        //         echo "Email déjà utilisé";
+        //     }
+
+        // }   
+        
+        // else if(isset($_POST['username']) && empty($_POST['username']))
+        // {
+        //     echo "Veuillez saisir un Pseudo";
+        // }
+        // else if(isset($_POST['registerEmail']) && empty($_POST['registerEmail']))
+        // {
+        //     echo "Veuillez saisir un Email";
+        // }
+        
+        // $users=$this->manager->saveUser($newUser);
+
+
     public function editUser(array $newUser)
     {
         
